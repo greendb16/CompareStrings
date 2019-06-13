1f3b24f2b1bc888c665fb6d4a66a64577f2272c4
greendb16 <greendb16@gmail.com>
2019-06-13 12:10:33 -0400

    first commit

---
+++ src/CompareStrings.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class CompareStrings {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How is the Weather?");
+        String answer = sc.nextLine();
+
+        if (answer.equalsIgnoreCase("rain")) {
+            System.out.println("Take your Umbrella!");
+        }
+            else if (answer.equalsIgnoreCase("win")){
+            System.out.println("Wear you jacket!");}
+            else if (answer.equalsIgnoreCase("snow"))}
+        }
+    }
+
+}
