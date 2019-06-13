92dae86dbf9f623c10f057607aee1b30bbc68744
greendb16 <greendb16@gmail.com>
2019-06-13 13:02:43 -0400

    final commit

--- src/CompareStrings.java
+++ src/CompareStrings.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 public class CompareStrings {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("How is the Weather?");
@@ -10,11 +10,15 @@ public class CompareStrings {
 
         if (answer.equalsIgnoreCase("rain")) {
             System.out.println("Take your Umbrella!");
+        } else if (answer.equalsIgnoreCase("windy")) {
+            System.out.println("Wear you jacket!");
+        } else if (answer.equalsIgnoreCase("snow")) {
+            System.out.println("Wear your jacket!");
+        } else if (answer.equalsIgnoreCase("stormy")) {
+            System.out.println("You should stay inside");
+        } else {
+            System.out.println("Enjoy your day!");
         }
-            else if (answer.equalsIgnoreCase("win")){
-            System.out.println("Wear you jacket!");}
-            else if (answer.equalsIgnoreCase("snow"))}
-        }
-    }
 
+    }
 }
